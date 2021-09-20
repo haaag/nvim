ccfaca5239612fa2e427619bae9f09d3a4cc4406
haaag <haag_@tutanota.com>
2021-09-20 20:49:15 -0300

    new python host path

--- lua/lsp/config/python-ls.lua
+++ lua/lsp/config/python-ls.lua
@@ -1,6 +1,6 @@
 --- python lsp simple config
 ----------------------------
-
+local HOME = vim.loop.os_homedir()
 local lsp = require 'lspconfig'
 local custom_attach = function(client)
     print("Python LSP started.");
@@ -8,7 +8,8 @@ end
 
 lsp.pyright.setup{on_attach=custom_attach}
 
-vim.g.python3_host_prog = 'python3'
+-- vim.g.python3_host_prog = 'python3'
+vim.g.python3_host_prog = HOME .. '/.local/debugpy/bin/python';
 
 -- no python2
 vim.g.loaded_python_provider = 0
