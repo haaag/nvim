8a40755594790cd4ca2e7217b7d0b697e75247ee
haaag <haag_@tutanota.com>
2021-11-29 19:44:42 -0300

    updated tokyonight config

--- lua/plugins/themes/tokyonight.lua
+++ lua/plugins/themes/tokyonight.lua
@@ -1,10 +1,12 @@
--- Lua
-vim.g.tokyonight_style = "storm"
--- vim.g.tokyonight_style = "day"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_comments = false
-vim.g.tokyonight_italic_keywords = true
-vim.g.tokyonight_hide_inactive_statusline = true
-vim.g.tokyonight_dark_sidebar = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-vim.cmd[[colorscheme tokyonight]]
+-- tokyonight
+local g = vim.g
+g.tokyonight_style = "storm"
+-- g.tokyonight_style = "day"
+g.tokyonight_italic_functions = true
+g.tokyonight_italic_comments = false
+g.tokyonight_italic_keywords = true
+-- g.tokyonight_hide_inactive_statusline = true
+g.tokyonight_dark_sidebar = true
+g.tokyonight_dark_float = true
+g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer", "tagbar" }
+vim.cmd [[ colorscheme tokyonight ]]
