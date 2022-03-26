33feabfde24043f2be8bc0074b920a957c41203f
haaag <haag_@tutanota.com>
2022-03-26 18:36:20 -0300

    lualine-bubble edit

--- lua/plugins/ui/lualine-bubble.lua
+++ lua/plugins/ui/lualine-bubble.lua
@@ -3,10 +3,10 @@
 -- MIT license, see LICENSE for more details.
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  return
+	return
 end
 
-local theme = require('plugins.themes.theme-colors').colors()
+local theme = require("plugins.themes.theme-colors").colors()
 
 -- stylua: ignore
 local colors = {
@@ -22,59 +22,63 @@ local colors = {
 }
 
 local bubbles_theme = {
-    normal = {
-        a = {fg = colors.black, bg = colors.blue},
-        b = {fg = colors.white, bg = colors.grey},
-        c = {fg = colors.black, bg = colors.black}
-    },
+	normal = {
+		a = { fg = colors.black, bg = colors.blue },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.black, bg = colors.black },
+	},
 
-    insert = {a = {fg = colors.black, bg = colors.blue}},
-    visual = {a = {fg = colors.black, bg = colors.cyan}},
-    replace = {a = {fg = colors.black, bg = colors.red}},
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
 
-    inactive = {
-        a = {fg = colors.white, bg = colors.black},
-        b = {fg = colors.white, bg = colors.black},
-        c = {fg = colors.black, bg = colors.black}
-    }
+	inactive = {
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.black, bg = colors.black },
+	},
 }
 
-lualine.setup{
-    options = {
-        -- theme = bubbles_theme,
-        theme = theme.lualine,
-        component_separators = '|',
-        section_separators = {left = '', right = ''},
-        disabled_filetypes = {'NvimTree', 'toggleterm', 'tagbar', 'vista', 'vista_kind'}
-    },
-    sections = {
-        lualine_a = {{'mode', separator = {left = ''}, right_padding = 2}},
-        lualine_b = {
-            'filename', {'branch', icon = ''}, {
-                'diff',
-                symbols = {added = ' ', modified = ' ', removed = ' '},
-                diff_color = {
-                    added = {fg = colors.green},
-                    modified = {fg = colors.blue},
-                    removed = {fg = colors.red}
-                }
-            }
-        },
-        lualine_c = {'fileformat'},
-        lualine_x = {},
-        lualine_y = {'filetype', 'progress'},
-        lualine_z = {
-            {'location', separator = {right = ''}, left_padding = 2}
-        }
-    },
-    inactive_sections = {
-        lualine_a = {'filename'},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {'location'}
-    },
-    tabline = {},
-    extensions = {}
-}
+lualine.setup({
+	options = {
+		theme = bubbles_theme,
+		-- theme = theme.lualine,
+		component_separators = "|",
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "NvimTree", "toggleterm", "tagbar", "vista", "vista_kind" },
+	},
+	sections = {
+		lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+		lualine_b = {
+			"filename",
+			{ "branch", icon = "" },
+			{
+				"diff",
+				symbols = { added = " ", modified = " ", removed = " " },
+				diff_color = {
+					added = { fg = colors.green },
+					modified = { fg = colors.blue },
+					removed = { fg = colors.red },
+				},
+			},
+		},
+		lualine_c = { "fileformat" },
+		lualine_x = {},
+		lualine_y = { "filetype", "progress" },
+		lualine_z = {
+			{ "location", separator = { right = "" }, left_padding = 2 },
+		},
+	},
+	inactive_sections = {
+		lualine_a = { "filename" },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "location" },
+	},
+	tabline = {},
+	extensions = {},
+})
+
+vim.cmd([[ set laststatus=3 ]])
