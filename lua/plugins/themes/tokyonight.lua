783843c6273f97837cc860211932414dc92ed79c
haaag <haag_@tutanota.com>
2022-04-17 23:09:23 -0300

    tokyonight theme

--- lua/plugins/themes/tokyonight.lua
+++ lua/plugins/themes/tokyonight.lua
@@ -7,10 +7,10 @@ end
 local g = vim.g
 g.tokyonight_style = "storm"
 -- g.tokyonight_style = "day"
+-- g.tokyonight_hide_inactive_statusline = true
 g.tokyonight_italic_functions = true
 g.tokyonight_italic_comments = false
 g.tokyonight_italic_keywords = true
--- g.tokyonight_hide_inactive_statusline = true
 g.tokyonight_dark_sidebar = true
 g.tokyonight_dark_float = true
 g.tokyonight_lualine_bold = true
