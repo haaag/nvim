399439333cba47dd8466cf066ee7be435d624002
haaag <haag_@tutanota.com>
2022-04-17 23:07:21 -0300

    added default lsp mappings

--- lua/lsp/config/lsp-defaults.lua
+++ lua/lsp/config/lsp-defaults.lua
@@ -1,18 +1,18 @@
 local M = {}
 
-
-M.custom_attach = function (client)
-    print("attached to " .. client.name);
+M.custom_attach = function(client, bufnr)
+	require("keys.mappings").lsp_keys(bufnr)
+	print("attached to " .. client.name)
 end
 
-M.custom_attach_no_formatting = function (client)
-    client.resolved_capabilities.document_formatting = false,
-    print("Attached to " .. client.name)
+M.custom_attach_no_formatting = function(client, bufnr)
+	require("keys.mappings").lsp_keys(bufnr)
+	client.resolved_capabilities.document_formatting = false, print("Attached to " .. client.name)
 end
 
-M.capabilities = function ()
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-    return capabilities
+M.capabilities = function()
+	capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+	return capabilities
 end
 
 return M
