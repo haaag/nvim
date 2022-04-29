37541d9e4a9ff316da541b941a5b6d9b97a580cf
haaag <haag_@tutanota.com>
2022-04-29 12:25:16 -0300

    migrating to new api

--- lua/lsp/config/deluge-ls.lua
+++ lua/lsp/config/deluge-ls.lua
@@ -1,35 +1,34 @@
 -- deluge zoho
-local defaults = require("lsp.config.lsp-defaults")
 local status_ok, lsp = pcall(require, "lspconfig")
 if not status_ok then
 	return
 end
 
+local cmd = vim.cmd
+local home = os.getenv("HOME")
+
+local defaults = require("lsp.config.lsp-defaults")
 local custom_attach = defaults.custom_attach
+
 local capabilities = defaults.capabilities()
 local configs = require("lspconfig.configs")
 
 -- Syntax highlight for Deluge files
-vim.cmd([[ autocmd BufNewFile,BufRead /*.dg setf deluge ]])
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = "dg",
--- 	callback = function()
--- 		vim.cmd([[ set filetype=deluge ]])
--- 	end,
--- })
+vim.api.nvim_create_autocmd("FileType,BufRead", {
+	pattern = "dg",
+	callback = function()
+		cmd([[ set filetype=deluge ]])
+		cmd([[ syntax on ]])
+	end,
+})
 
 -- Check if it's already defined for when reloading this file.
 if not configs.deluge then
 	configs.deluge = {
 		default_config = {
-			-- cmd = { "/home/hh/apps/github/deluge/Deluge-Language-Parser/deluge-server" },
-			cmd = { "/home/hh/apps/dev/deluge-language-parser/deluge-server" },
+			cmd = { home .. "/apps/dev/deluge-language-parser/deluge-server" },
 			filetypes = { "deluge" },
 			root_dir = lsp.util.root_pattern(".git", "deluge", "projects"),
-			--[[ root_dir = function(fname)
-                return lsp.util.find_git_ancestor(fname) or
-                vim.loop.os_homedir()
-            end, ]]
 			settings = {},
 		},
 	}
