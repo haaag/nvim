b22428f8439554c4631a8214e24a8aa85c139ebd
haaag <haag_@tutanota.com>
2022-04-29 12:26:45 -0300

    migrating to new api

--- lua/lsp/config/tsserver-ls.lua
+++ lua/lsp/config/tsserver-ls.lua
@@ -5,23 +5,31 @@ if not status_ok then
 	return
 end
 
---[[ local custom_attach = function(client)
-	client.resolved_capabilities.document_formatting = false,
-    print("Attached to " .. client.name)
-end ]]
-
 local custom_attach = defaults.custom_attach_no_formatting
 local capabilities = defaults.capabilities()
 
--- javascript
-vim.cmd([[ autocmd FileType javascript setlocal shiftwidth=2 tabstop=2 ]])
-vim.cmd([[ autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll ]])
+-- autocommands
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "javascript",
+    callback = function ()
+        vim.cmd([[ setlocal shiftwidth=2 tabstop=2 ]])
+    end
+})
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = {"tsx", "ts", "jsx", "js"},
+    callback = function ()
+        vim.cmd([[ EslintFixAll ]])
+    end
+})
+
+-- eslint
 lsp.eslint.setup{
     capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
     on_attach=custom_attach
 }
 
+-- tsserver
 lsp.tsserver.setup({
 	-- capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 	capabilities = capabilities,
