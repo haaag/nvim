4cd52daaed40345f4eda6e4951bcb220ee59a6f5
haaag <haag_@tutanota.com>
2022-07-14 18:07:19 -0300

    new plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,8 @@
--- packer
+-- packer plugins
 ---------
 local fn = vim.fn
 
+-- packer: {{{
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
@@ -35,6 +36,8 @@ return packer.startup(function()
 	}
 	-- Packer can manage itself as an optional plugin
 	use({ "wbthomason/packer.nvim" })
+	-- }}}
+	-- lsp: {{{
 
 	-- lspconfig
 	use({
@@ -52,10 +55,10 @@ return packer.startup(function()
 		run = ":TSUpdate",
 	})
 
-	use({
+	--[[ use({
 		"nvim-treesitter/nvim-treesitter-context",
 		requires = { "nvim-treesitter/nvim-treesitter" },
-	})
+	}) ]]
 
 	-- ts-context-commentstring
 	use({
@@ -72,17 +75,18 @@ return packer.startup(function()
 	})
 
 	-- auto tags
-	use({ "windwp/nvim-ts-autotag", after = "nvim-cmp" })
+	use({ "windwp/nvim-ts-autotag" })
 
 	-- treesitter-refactor
 	use({ "nvim-treesitter/nvim-treesitter-refactor", after = "nvim-treesitter" })
 
-    -- treesitter-textobjects
-    use({
-        "nvim-treesitter/nvim-treesitter-textobjects",
-		tequires = { "nvim-treesitter/nvim-treesitter" },
-        after = "nvim-treesitter"
-    })
+	-- treesitter-textobjects {{{2
+	use({
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		requires = { "nvim-treesitter/nvim-treesitter" },
+		after = "nvim-treesitter",
+	})
+	-- }}}
 
 	-- null-ls
 	use({
@@ -95,9 +99,7 @@ return packer.startup(function()
 	use({
 		"ahmedkhalf/project.nvim",
 		config = function()
-			require("project_nvim").setup({
-				patterns = { ".env", ".git", "deluge", "app", "projects" },
-			})
+			require("project_nvim").setup({})
 		end,
 	})
 
@@ -107,16 +109,49 @@ return packer.startup(function()
 		config = function()
 			local g = vim.g
 			g.vista_sidebar_width = 48
-            -- g.vista_default_executive = 'nvim_lsp'
-            -- g.vista_echo_cursor_strategy = 'floating_win'
+			-- g.vista_default_executive = "nvim_lsp"
+			-- g.vista_echo_cursor_strategy = "floating_win"
 		end,
-		cmd = "Vista",
+		-- cmd = "Vista",
 	})
 
-	-- use({
-	-- 	"simrat39/symbols-outline.nvim",
-	-- 	config = [[require('plugins.tools.symbols-outline')]],
-	-- })
+	-- lsp fidget
+	use({
+		"j-hui/fidget.nvim",
+		--[[ config = function()
+			require("fidget").setup({
+				text = { spinner = "moon" }, -- grow_vertical, bouncing_bar, pipe
+				window = { winblend = 100 },
+			})
+		end, ]]
+		config = function()
+			require("fidget").setup({
+				-- text = { spinner = "moon" }, -- grow_vertical, bouncing_bar, pipe
+                text = {spinner = 'dots_scrolling'},
+                window = { blend = 0 },
+            })
+		end,
+	})
+
+	use({
+		"danymat/neogen",
+		config = function()
+			require("neogen").setup({})
+		end,
+		requires = "nvim-treesitter/nvim-treesitter",
+	})
+
+	use({
+		"narutoxy/dim.lua",
+		requires = { "nvim-treesitter/nvim-treesitter", "neovim/nvim-lspconfig" },
+		config = function()
+			require("dim").setup({
+				disable_lsp_decorations = true,
+			})
+		end,
+	})
+	-- }}}
+	-- utilities: {{{
 
 	-- Plugin to manipulate character pairs quickly
 	use({ "machakann/vim-sandwich", event = "VimEnter" })
@@ -138,126 +173,6 @@ return packer.startup(function()
 		end,
 	})
 
-	-- onedark
-	--[[ use({
-		"navarasu/onedark.nvim",
-		config = function()
-			vim.g.one_nvim_transparent_bg = true
-			require("onedark").setup({
-				ending_tildes = true,
-				style = "dark",
-			})
-			require("onedark").load()
-		end,
-	}) ]]
-
-	-- tokyonight
-	-- use({
-	-- 	"folke/tokyonight.nvim",
-	-- 	config = [[require('plugins.themes')]],
-	-- })
-
-	-- gruvbox
-	use({
-		"ellisonleao/gruvbox.nvim",
-		config = [[require('plugins.themes.gruvbox-nvim')]],
-	})
-
-	-- gruvbox-community
-	-- use({
-	-- 	"gruvbox-community/gruvbox",
-	-- 	config = [[require('plugins.themes.gruvbox-community')]],
-	-- })
-
-	-- gruvbox-baby
-	-- use({
-	--     "luisiacc/gruvbox-baby",
-	--     config = [[require('plugins.themes.gruvbox-baby')]]
-	-- })
-
-	-- gruvbox8
-	-- use({
-	--     "lifepillar/vim-gruvbox8",
-	--     config = [[require('plugins.themes.gruvbox8')]]
-	-- })
-
-	-- kanagawa
-	-- use({
-	--     "rebelot/kanagawa.nvim",
-	--     config = function ()
-	--         vim.cmd("colorscheme kanagawa")
-	--     end
-	-- })
-
-	-- statusline
-	-- use({
-	-- 	"nvim-lualine/lualine.nvim",
-	-- 	requires = { "kyazdani42/nvim-web-devicons" },
-	-- 	config = [[require('plugins.ui.lualine-bubble')]],
-	-- })
-
-	-- statusline - tabline
-	use({
-		"tamton-aquib/staline.nvim",
-		config = "require('plugins.ui.staline-simple')",
-	})
-
-	-- statusline
-	--[[ use({
-		"beauwilliams/statusline.lua",
-		config = function()
-			local statusline = require("statusline")
-			statusline.tabline = false
-		end,
-	}) ]]
-
-	-- code completion
-	use({
-		"hrsh7th/nvim-cmp",
-		requires = {
-			{ "neovim/nvim-lspconfig" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-			{ "hrsh7th/cmp-vsnip" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "f3fora/cmp-spell" },
-			{ "rafamadriz/friendly-snippets" },
-			{ "hrsh7th/cmp-nvim-lsp-signature-help" },
-			{
-				"hrsh7th/vim-vsnip",
-				config = function()
-					vim.g.vsnip_snippet_dir = "$HOME/.config/nvim/vsnip"
-				end,
-			},
-			{ "lukas-reineke/cmp-under-comparator" },
-			{ "lukas-reineke/cmp-rg" },
-		},
-		-- config = [[require('lsp.code.cmp-new')]],
-		config = [[require('lsp.code.cmp')]],
-	})
-
-	-- completion icons
-	use({ "onsails/lspkind-nvim" })
-
-	-- auto pair []
-	use({
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup()
-		end,
-		after = "nvim-cmp",
-	})
-
-	-- comment
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-		after = "nvim-cmp",
-	})
-
 	-- telescope
 	use({
 		"nvim-telescope/telescope.nvim",
@@ -275,6 +190,7 @@ return packer.startup(function()
 	use({
 		"akinsho/nvim-bufferline.lua",
 		config = [[require('plugins.ui.bufferline')]],
+		disable = false,
 	})
 
 	-- maximizer
@@ -288,43 +204,6 @@ return packer.startup(function()
 
 	-- better buffer del
 	use({ "ojroques/nvim-bufdel", cmd = "BufDel" })
-
-	-- git
-	use({
-		"lewis6991/gitsigns.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = [[require('plugins.tools.gitsings')]],
-		event = "BufRead",
-	})
-
-	-- smooth scroll
-	use({
-		"karb94/neoscroll.nvim",
-		config = [[require('plugins.ui.neoscroll')]],
-		event = "WinScrolled",
-	})
-
-	-- display icons
-	use({
-		"kyazdani42/nvim-web-devicons",
-		config = [[require('plugins.ui.icons')]],
-	})
-
-	-- colorizer
-	use({ "norcalli/nvim-colorizer.lua", cmd = "ColorizerToggle" })
-
-	-- highlight todo comments
-	use({
-		"folke/todo-comments.nvim",
-		requires = "nvim-lua/plenary.nvim",
-		config = [[require('lsp.code.todo-comments')]],
-	})
-
-	-- indent highlight
-	use({
-		"lukas-reineke/indent-blankline.nvim",
-		config = [[require('lsp.code.indent-blankline')]],
-	})
 
 	-- syntax highlight for sxhkd
 	use({ "baskerville/vim-sxhkdrc", ft = { "sxhkdrc" } })
@@ -369,14 +248,126 @@ return packer.startup(function()
 		config = [[require('plugins.tools.neo-tree')]],
 	})
 
-	-- CursorHold bug: https://github.com/neovim/neovim/issues/12587
-	use({ "antoinemadec/FixCursorHold.nvim" })
+	-- syntax for kitty.conf
+	use({ "fladson/vim-kitty", ft = { "kitty" } })
 
-	-- impatient
-	use({ "lewis6991/impatient.nvim", config = [[require('plugins.tools.impatient')]] })
+	-- sessions
+	use({
+		"tpope/vim-obsession",
+	})
 
-	-- better filetype.vim
-	use({ "nathom/filetype.nvim" })
+	-- rest.nvim
+	use({
+		"NTBBloodbath/rest.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+
+	use({
+		"mbbill/undotree",
+		cmd = "UndotreeToggle",
+		config = function()
+			vim.g.undotree_TreeNodeShape = "◦" -- Alternative: '◉'
+			vim.g.undotree_SetFocusWhenToggle = 1
+		end,
+	})
+
+	-- }}}
+	-- themes: {{{
+	-- onedark
+	--[[ use({
+		"navarasu/onedark.nvim",
+		config = function()
+			vim.g.one_nvim_transparent_bg = true
+			require("onedark").setup({
+				ending_tildes = true,
+				style = "dark",
+			})
+			require("onedark").load()
+		end,
+	}) ]]
+
+	-- tokyonight
+	-- use({
+	-- 	"folke/tokyonight.nvim",
+	-- 	config = [[require('plugins.themes.tokyonight')]],
+	-- })
+
+	-- gruvbox
+	-- use({
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	config = [[require('plugins.themes.gruvbox-nvim')]],
+	-- })
+
+	-- gruvbox-community
+	-- use({
+	-- 	"gruvbox-community/gruvbox",
+	-- 	config = [[require('plugins.themes.gruvbox-community')]],
+	-- })
+
+	-- kanagawa
+	use({
+		"rebelot/kanagawa.nvim",
+		config = [[require('plugins.themes.kanagawa')]],
+	})
+	-- }}}
+	-- code: {{{
+
+	-- completion
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			{ "neovim/nvim-lspconfig" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "hrsh7th/cmp-vsnip" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "f3fora/cmp-spell" },
+			{ "rafamadriz/friendly-snippets" },
+			{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+			{
+				"hrsh7th/vim-vsnip",
+				config = function()
+					vim.g.vsnip_snippet_dir = "$HOME/.config/nvim/vsnip"
+				end,
+			},
+			{ "lukas-reineke/cmp-under-comparator" },
+			{ "lukas-reineke/cmp-rg" },
+		},
+		config = [[require('lsp.code.cmp')]],
+	})
+
+	-- completion
+	--[[ use({
+		"ms-jpq/coq_nvim",
+		branch = "coq",
+		event = "InsertEnter",
+		run = ":COQdeps",
+		config = function()
+			require("lsp.code.coq").setup()
+		end,
+		requires = {
+			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
+			{ "ms-jpq/coq.thirdparty", branch = "3p", module = "coq_3p" },
+		},
+		disable = false,
+	}) ]]
+
+	-- auto pair []
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	})
+
+	-- comment
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
 
 	-- trouble
 	-- use({
@@ -386,8 +377,63 @@ return packer.startup(function()
 	-- 	cmd = { "TroubleToggle", "Trouble", "TroubleClose" },
 	-- })
 
-	-- syntax for kitty.conf
-	use({ "fladson/vim-kitty", ft = { "kitty" } })
+	-- }}}
+	-- git {{{
+
+	-- gitsings
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = [[require('plugins.tools.gitsings')]],
+		event = "BufRead",
+	})
+
+	-- diffview
+	use({
+		"sindrets/diffview.nvim",
+		requires = "nvim-lua/plenary.nvim",
+	})
+
+	-- neogit
+	use({
+		"TimUntersberger/neogit",
+		config = function()
+			require("neogit").setup({
+				integrations = {
+					diffview = true,
+				},
+			})
+		end,
+	})
+
+	-- }}}
+	-- ui: {{{
+
+	-- icons
+	use({
+		"kyazdani42/nvim-web-devicons",
+		config = [[require('plugins.ui.icons')]],
+	})
+
+	use({
+		"yamatsum/nvim-nonicons",
+		requires = { "kyazdani42/nvim-web-devicons" },
+	})
+	-- colorizer
+	use({ "norcalli/nvim-colorizer.lua", cmd = "ColorizerToggle" })
+
+	-- highlight todo comments
+	use({
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = [[require('lsp.code.todo-comments')]],
+	})
+
+	-- indent highlight
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = [[require('lsp.code.indent-blankline')]],
+	})
 
 	-- pretty-fold
 	use({
@@ -399,30 +445,89 @@ return packer.startup(function()
 		end,
 	})
 
-	-- sessions
-	use({
-		"tpope/vim-obsession",
-	})
-
-	-- rest.nvim
-	use({
-		"NTBBloodbath/rest.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = [[require('plugins.tools.rest')]],
-	})
-
 	-- list chars
 	use({
 		"tjdevries/cyclist.vim",
 	})
 
-    use({
-        "SmiteshP/nvim-gps",
-        requires = "nvim-treesitter/nvim-treesitter"
-    })
+	use({
+		"SmiteshP/nvim-gps",
+		requires = "nvim-treesitter/nvim-treesitter",
+		config = [[require('plugins.ui.gps')]],
+	})
 
 	-- ui
 	-- use({
 	--     "stevearc/dressing.nvim"
 	-- })
+
+	-- statusline - tabline
+	use({
+		"tamton-aquib/staline.nvim",
+		config = "require('plugins.ui.staline-simple')",
+	})
+
+	-- smooth scroll
+	use({
+		"karb94/neoscroll.nvim",
+		config = [[require('plugins.ui.neoscroll')]],
+		event = "WinScrolled",
+	})
+	-- }}}
+	-- performance: {{{
+
+	-- CursorHold bug: https://github.com/neovim/neovim/issues/12587
+	use({
+		"antoinemadec/FixCursorHold.nvim",
+		event = { "BufRead", "BufNewFile" },
+		config = function()
+			vim.g.cursorhold_updatetime = 100
+		end,
+	})
+
+	-- impatient
+	use({ "lewis6991/impatient.nvim", config = [[require('plugins.tools.impatient')]] })
+
+	-- startuptime
+	use({ "dstein64/vim-startuptime" })
+
+	-- better filetype.vim
+	use({ "nathom/filetype.nvim" })
+	-- }}}
+	-- debugging {{{
+
+	-- nvim-dap
+	use({
+		"mfussenegger/nvim-dap",
+		config = [[require('plugins.tools.nvim-dap')]],
+	})
+
+	use({
+		"rcarriga/nvim-dap-ui",
+		requires = { "mfussenegger/nvim-dap" },
+	})
+
+	use({
+		"theHamsta/nvim-dap-virtual-text",
+		requires = { "mfussenegger/nvim-dap" },
+	})
+
+	use({
+		"nvim-telescope/telescope-dap.nvim",
+		requires = {
+			{ "mfussenegger/nvim-dap" },
+			{ "nvim-telescope/telescope.nvim" },
+		},
+	})
+
+	use({
+		"mfussenegger/nvim-dap-python",
+		requires = { "mfussenegger/nvim-dap" },
+		config = function()
+			require("dap-python").setup("~/.local/debugpy/bin/python")
+		end,
+	})
+	-- }}}
 end)
+
+-- vim: fdm=marker fmr={{{,}}}:
