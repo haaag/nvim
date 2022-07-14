1ebf83cbbe1f6bafb87039bf686b66a442b3d479
haaag <haag_@tutanota.com>
2022-07-14 18:07:42 -0300

    init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- vim.lsp.set_log_level("debug")
 --
+-- require('impatient')
 require("plugins.tools.impatient")
 
 -- Do not source the default filetype.vim
@@ -10,8 +11,11 @@ local modules = {
 	"keys",
 	"lsp",
 	"plugins",
+	"plugins.tools",
 }
 
 for i = 1, #modules, 1 do
 	pcall(require, modules[i])
 end
+
+vim.lsp.set_log_level("debug")
