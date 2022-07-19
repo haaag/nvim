162485a331d52fd1c7f6abd2b9dcb5457e90908d
haaag <haag_@tutanota.com>
2022-07-19 12:00:41 -0300

    cleanup null-ls config file.

--- lua/lsp/config/null-ls.lua
+++ lua/lsp/config/null-ls.lua
@@ -12,17 +12,11 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		-- formatting.black.with({ extra_args = { "--fast", "--line-length=100" } }),
-		-- formatting.black.with({ extra_args = { "--fast" } }),
-		-- formatting.isort,
-		-- diagnostics.flake8.with({ extra_args = { "--ignore=E501" } }),
 		formatting.prettier,
 		formatting.stylua,
 		formatting.shfmt,
 		formatting.shellharden,
 		formatting.eslint_d,
-		-- diagnostics.mypy.with({ extra_args = { "--ignore-missing-imports" } }),
 		diagnostics.shellcheck,
 		diagnostics.eslint_d,
 	},
