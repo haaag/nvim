4095fdf68030849f9bf723ad360de9aa2c9eac53
haaag <haag_@tutanota.com>
2022-07-28 12:36:33 -0300

    fix(clean-up): deleting junk files

--- lua/plugins/ui/test.lua
+++
@@ -1,8 +0,0 @@
-local extract_hl = function(hl, fore)
-    return ("#%06x"):format(
-        vim.api.nvim_get_hl_by_name(hl, true)[fore and 'background' or 'foreground'] or 255
-    )
-end
--- local test = vim.api.nvim_get_hl_by_name("StalinePavel", true)
--- vim.pretty_print(test)
-vim.pretty_print(extract_hl("StalinePavel"))
